2916ea72340b2918cd1aac6e77212dfdade7d802
Raghav Mittal <mittalraghav45@gmail.com>
2026-09-21 22:52:09 +0100

    Fix stylesheet import for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './style.css';
+import './styles.css';
 
 const healthcareThumbnail = 'https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?auto=format&fit=crop&w=1200&q=85';
 
